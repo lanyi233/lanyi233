955e696adeae34c75594147c56bd53a564db4584
lanyi233 <lanyi233@qq.com>
2024-03-23 08:01:32 +0800

    优化 sub 提示

--- script/substore/install.sh
+++ script/substore/install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 echo "此脚本不保证可用性，如想停止执行请 Ctrl+C"
 if [ ! -d "$HOME/substore" ]; then
-echo "正在安装sub-store，node"
+echo "正在安装sub-store，node,Nginx(前端服务)"
 
-  apt update -y && apt install nodejs -y
+  apt update -y && apt install nodejs nginx -y
 
   mkdir -p "$HOME/substore" && cd "$HOME/substore"
 
-  curl -s -o update.sh "https://raw.githubusercontent.com/lanyi233/lanyi233/master/script/substore/update.sh" && chmod +x update.sh
-
-  curl -s -o sub "https://raw.githubusercontent.com/lanyi233/lanyi233/master/script/substore/sub" && chmod +x sub && mv "./sub" "$HOME/../usr/bin" 
+  curl -o update.sh "https://raw.githubusercontent.com/lanyi233/lanyi233/master/script/substore/update.sh" && chmod +x update.sh
   
+  curl -o update.sh "https://raw.githubusercontent.com/lanyi233/lanyi233/master/script/substore/update.sh" && chmod +x update_end.sh
+
+  curl -o sub "https://raw.githubusercontent.com/lanyi233/lanyi233/master/script/substore/sub" && chmod +x sub && mv "./sub" "$HOME/../usr/bin" 
+
     sub -u
 
-echo "\n\n安装完成，使用 sub 运行(-h 可隐藏token日志，-u可更新sub-store)"
+echo "\n\n安装完成，在终端运行 sub 以运行"
 
 rm install.sh
 else
-    echo "你已安装sub-store，请使用 sub 运行(-h 可隐藏token日志，-u可更新sub-store)"
+    echo "你已安装sub-store，请在终端运行 sub"
     rm install.sh
 fi
